095e71ace20672fc7aa041f18fadae42f2a30563
avadhanisanghamitra <138146055+avadhanisanghamitra@users.noreply.github.com>
2023-07-26 17:55:42 +0530

    Update rolldice.py

--- rolldice.py
+++ rolldice.py
@@ -3,6 +3,10 @@ min_value=1
 max_value=6
 roll_again="y"
 while roll_again=="y" or roll_again=="yes":
-    print("Rolling the dice....\n You Number is ",random.randint(min_value,max_value))
+    r1=random.randint(min_value,max_value)
+    r2=random.randint(min_value,max_value)
+    print("Rolling the dice....\n You Numbers are ",r1,r2)
+    if r1==r2:
+        print("Hurray! You WON")
     roll_again=input("Want to roll the Dice Again(y/n)or(yes/no):")
 print("The Game has ended , Thank you!")
